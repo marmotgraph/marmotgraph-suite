ddcc44af9905a7f8715ed1a74fd2be6e0f12fe47
Oliver Schmid <oliver.schmid@cscs.ch>
2026-01-22 09:57:12 +0100

    combine vitest and playwright

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -7,11 +7,10 @@ export default defineConfig({
     timeout: 120 * 1000,
     reuseExistingServer: false,
   },
+  testDir: "./tests/e2e", // only run tests from this directory
   use: {
     baseURL: "http://localhost:4173",
     headless: true,
   },
   reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
 });
-
-
